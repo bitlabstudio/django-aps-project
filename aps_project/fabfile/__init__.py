d2c38ad2e68bfec19fca7d04ca3e2ddc8772ce0d
Martin Brochhaus <mbrochh@gmail.com>
2014-01-14 13:46:59 +0800

    Added submodules

--- aps_project/fabfile/__init__.py
+++ aps_project/fabfile/__init__.py
@@ -1,6 +1,7 @@
 # flake8: noqa
-from aps_project import settings
-from django.core.management import setup_environ
-setup_environ(settings)
+import os
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "aps_project.settings")
 
 from development_fabfile.fabfile import *
