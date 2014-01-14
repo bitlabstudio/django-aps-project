e606709dac1e2663ad2a45630d696e30fd2d10df
Martin Brochhaus <mbrochh@gmail.com>
2014-01-14 16:06:42 +0800

    Fixed debug toolbar settings

--- aps_project/aps_project/settings/installed_apps/debug_toolbar.py
+++ aps_project/aps_project/settings/installed_apps/debug_toolbar.py
@@ -1,5 +1,12 @@
 """Settings for the ``debug_toolbar`` app."""
 DEBUG_TOOLBAR_CONFIG = {
     'INTERCEPT_REDIRECTS': False,
-    'SHOW_TOOLBAR_CALLBACK': lambda x: True,
+    'SHOW_TOOLBAR_CALLBACK': 'aps_project.settings.installed_apps.debug_toolbar.show_toolbar',  # NOQA
 }
+
+def show_toolbar(request):
+    return True
+
+
+def hide_toolbar(request):
+    return False
