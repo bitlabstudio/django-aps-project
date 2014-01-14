313d70128b50dbbc03d472596d2e1a88fef84162
Martin Brochhaus <mbrochh@gmail.com>
2014-01-14 13:29:08 +0800

    Initial commit

---
+++ aps_project/aps_project/settings/installed_apps/__init__.py
@@ -0,0 +1,29 @@
+"""Installed apps for the aps_project project."""
+DJANGO_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+]
+
+EXTERNAL_APPS = [
+    'admin_honeypot',
+    'debug_toolbar',
+    'django_libs',
+    'honeypot_signals',
+    'mailer',
+    'south',
+    'rapid_prototyping',
+]
+
+INTERNAL_APPS = [
+    'aps_project',
+]
+
+INSTALLED_APPS = DJANGO_APPS + EXTERNAL_APPS + INTERNAL_APPS
+
+from .rapid_prototyping import *  # NOQA
+from .debug_toolbar import *  # NOQA
---
+++ aps_project/aps_project/settings/installed_apps/rapid_prototyping.py
@@ -0,0 +1,3 @@
+"""Settings for the rapid_prototyping app."""
+RAPID_PROTOTYPING_SPRINTS_MODULE = 'aps_project.context.sprints.sprints'
+RAPID_PROTOTYPING_HOURLY_RATE = 70
